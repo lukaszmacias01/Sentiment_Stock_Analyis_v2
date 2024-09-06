e0ab99a1b12ec0ecc50b2beec44766a4a438815f
lukaszmacias01 <lukasz.macias.01@gmail.com>
2024-09-06 14:31:10 +0200

    Update 5 data analysis price sentiment.py

--- 5 data analysis price sentiment.py
+++ 5 data analysis price sentiment.py
@@ -1,13 +1,2 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  5 16:49:31 2024
 
-@author: Lukasz Macias
-"""
-
-import pandas as pd 
-
-df = pd.read_excel('TSLA_sentiment.xlsx')
-
-df['time'].min()
 
